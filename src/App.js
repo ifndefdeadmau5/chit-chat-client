3e06a2d4e6d533f2c3ae02b74620af934dfac510
Trevor <trevorme91@gmail.com>
2019-07-14 21:15:29 +0900

    Hmmm

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
 import logo from './logo.svg';
 import './App.css';
 
+const socket = io('http://localhost:3000');
+
 function App() {
+  const [text, setText] = useState('');
+  const [messages, setMessages] = useState([]);
+
+  const handleChange = e => {
+    setText(e.target.value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    socket.emit('chat message', text);
+    setText('');
+  };
+
+  useEffect(() => {
+    socket.on('chat message', function(newMsg) {
+      setMessages(prev => [...prev, newMsg]);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header class="App-header">
+      <img src={logo} className="App-header" alt="logo" />
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={text} onChange={handleChange} />
+        <button type="submit">Go</button>
+      </form>
+      <ul>
+        {messages.map(v => (
+          <li>{v}</li>
+        ))}
+      </ul>
       </header>
     </div>
   );
