ac44c6245e3919d302b109127285b3b261911a2e
박성민 <trevorme91@gmail.com>
2019-07-22 16:39:42 +0900

    Name users

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import io from 'socket.io-client';
 import logo from './logo.svg';
 import './App.css';
 
-const socket = io('http://localhost:3000');
+const socket = io('http://121.254.173.45:3333');
 
 function App() {
   const [text, setText] = useState('');
+  const [username, setUsername] = useState('');
+  const [submitted, setSubmitted] = useState(false);
   const [messages, setMessages] = useState([]);
 
   const handleChange = e => {
@@ -15,29 +17,51 @@ function App() {
 
   const handleSubmit = e => {
     e.preventDefault();
-    socket.emit('chat message', text);
+    socket.emit('new message', text);
+    setMessages(prev => [...prev, { username, message: text }]);
     setText('');
   };
 
   useEffect(() => {
-    socket.on('chat message', function(newMsg) {
-      setMessages(prev => [...prev, newMsg]);
+    socket.on('new message', function(messageObj) {
+      setMessages(prev => [...prev, messageObj]);
     });
   }, []);
+
+  if (!submitted) {
+    return (
+      <div style={{ padding: '1em' }}>
+        <p>insert username</p>
+        <input
+          type="text"
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+        />
+        <button
+          onClick={() => {
+            setSubmitted(true);
+            socket.emit('add user', username);
+          }}
+        >
+          submit
+        </button>
+      </div>
+    );
+  }
 
   return (
     <div className="App">
       <header class="App-header">
-      <img src={logo} className="App-header" alt="logo" />
-      <form onSubmit={handleSubmit}>
-        <input type="text" value={text} onChange={handleChange} />
-        <button type="submit">Go</button>
-      </form>
-      <ul>
-        {messages.map(v => (
-          <li>{v}</li>
-        ))}
-      </ul>
+        <img style={{ width: 400, height: 400 }} src={logo} alt="logo" />
+        <form onSubmit={handleSubmit}>
+          <input type="text" value={text} onChange={handleChange} />
+          <button type="submit">Go</button>
+        </form>
+        <ul style={{ textAlign: 'left', listStyle: 'none' }}>
+          {messages.map(({ username, message }) => (
+            <li>{`${username}: ${message}`}</li>
+          ))}
+        </ul>
       </header>
     </div>
   );
